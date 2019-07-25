374cc484622ae76e59881337dd9fd0e6ed5710c4
Collin Johnson <git@collin.fun>
2019-07-24 20:40:48 -0700

    Stub out script for initializing Discord bot

--- minecraft.js
+++ minecraft.js
@@ -1,6 +1,6 @@
 var spawn = require('child_process').spawn;
 var backup = require('./backup');
-
+var discord = require('./discord');
 
 
 var accessKeyID = process.env['MINECRAFT_S3_accessKeyID'];
@@ -8,6 +8,12 @@ var secretAccessKey = process.env['MINECRAFT_S3_secretAccessKey'];
 
 var bucketName = process.env['MINECRAFT_S3_bucketName'];
 var region = process.env['MINECRAFT_S3_region'] || 'us-west-2';
+
+var discordToken = process.env['MINECRAFT_DISCORD_token'];
+
+if (discordToken) {
+    discord.init(discordToken);
+}
 
 backup.init( { accessKeyID, secretAccessKey, region } );
 
